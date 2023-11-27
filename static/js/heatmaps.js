ded1ebe0e5c42a9b017a3c87558a4ac92de75047
S-Bonillas <137961214+S-Bonillas@users.noreply.github.com>
2023-11-26 18:22:22 -0800

    Updated Index.html, heatmaps.js, and python code
    - Cleanup in Python
    - Updated JS formatting and framing in Index and Style.css
    - Updated version of Heatmaps.js (first draft on map visualization)

--- static/js/heatmaps.js
+++ static/js/heatmaps.js
@@ -1,72 +1,26 @@
-
-// Define tile layers for different styles
-let darkMap = L.tileLayer(
-    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: 'Map data: &copy; OpenStreetMap contributors, Tiles by Stamen Design'
-});
-
-let topoMap = L.tileLayer(
-    "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
-    attribution: 'Map data: &copy; OpenTopoMap contributors, Tiles by Stamen Design'
-});
-
-// Define the default base map
-let defaultMap = streetMap;
-
 // Define the map
-let map = L.map("map", {
-    center: [40.7, -94.5],
-    zoom: 3,
-    layers: [defaultMap]
-});
+let map = L.map('map').setView([25, -30], 2.5);
+
+let darkMap = L.tileLayer(
+    'https://api.maptiler.com/maps/toner-v2/{z}/{x}/{y}.png?key=gyY5igTqRQf5an0OvTN6', {
+    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+}).addTo(map);
+
+let marker = L.marker([51.5, -0.09]).addTo(map);
 
 // Combine formatting
 let baseMaps = {
-    "Street Map": streetMap,
+    "Dark Map": darkMap,
     "Topo Map": topoMap
 };
 
-// Adding Tectonic Map Data
-let tectonicPlates = new L.LayerGroup();
-let earthquakes = new L.LayerGroup();
-
-let overlays = {
-    "Tectonic Plates": tectonicPlates,
-    "Earthquakes": earthquakes
-};
 
 // Add layer control to switch between base maps and overlays
 L.control.layers(baseMaps, overlays).addTo(map);
 
 
-// Creating color function
-function getColor(depth) {
-    if(depth > 90) {
-        return "#ea2c2c"  
-    } else if(depth > 70) {
-        return "#ea822c"
-    } else if(depth > 50) {
-        return "#ee9c00"
-    } else if(depth > 30) {
-        return "#eecc00"
-    } else if(depth >10) {
-        return "#d4ee00"
-    }
-    else {
-        return "#98ee00"
-    }
-}
-
-// Creating radius function
-function getRadius(magnitude){
-    if(magnitude === 0) {
-        return 1
-    }    
-    return magnitude * 4
-}
-
 // Calling the GeoJSON data from the USGS api feed
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+d3.json("https://api.nobelprize.org/2.1/nobelPrizes?offset=1&limit=100000&sort=desc&nobelPrizeYear=1901&yearTo=2023&format=json").then(function(data) {
     console.log(data);
     function styleInfo(feature) {
         return {
@@ -103,29 +57,4 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         position: "bottomright"
     });
 
-    // Formatting the Legend
-    legend.onAdd = function(){
-        let container = L.DomUtil.create("div", "info legend");
-        let grades = [-10, 10, 30, 50, 70, 90];
-        let colors = ['#98ee00', '#d4ee00', '#eecc00', '#ee9c00', '#ea822c', '#ea2c2c'];
-        for (let index = 0; index < grades.length; index++) {
-            container.innerHTML += `<i style="background: ${colors[index]}"></i> ${grades[index]}+ <br>`
-        }
-        return container;
-    };
-
-    legend.addTo(map);
-
-    // Pulling Tectonic map from the the Fraxen boundaries json
-    d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function(plateData){
-        L.geoJson(plateData, {
-            color: "orange",
-            width: 3,
-        }).addTo(tectonicPlates);
-
-        tectonicPlates.addTo(map);
-
-    });
 });
-
-
