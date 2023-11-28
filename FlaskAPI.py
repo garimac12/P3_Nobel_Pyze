1b62044fbf71d8aec46fdcebe5cb907592b60074
Sequoia Boubion-Mckay <111154276+Sequoiabm@users.noreply.github.com>
2023-11-27 22:19:55 -0800

    Updated app routes
    Added merged data routes (awards and laureates), (All tables merged)

--- FlaskAPI.py
+++ FlaskAPI.py
@@ -102,6 +102,11 @@ def home():
         f"/Awards<br/>"
         f"/Prizes<br/>"
         f"/Orgs<br/>"
+        f"/LaureatesAndAwards<br/>"
+        f"/AllTables<br/>"
+        
+
+        
     )
 
 #------------------------------------------------------------------------
@@ -184,6 +189,101 @@ def get_orgs():
 
     return jsonify({'Orgs': orgs_list})
 
+
+#------------------------------------------------------------------------
+#                       Laureates And Awards App route
+#------------------------------------------------------------------------
+@app.route("/LaureatesAndAwards")
+def get_laureates_and_awards():
+    # Perform a join operation to get data from both Laureates and Awards tables
+    # Assuming there's a foreign key relationship between Laureates and Awards
+    joined_data = db.session.query(Laureate, Award).filter(Laureate.Laureate_id == Award.Laureate_id).all()
+
+    # Format the joined data for JSON response
+    merged_list = [
+        {
+            'Laureate_id': laureate.Laureate_id,
+            'Org_Type': laureate.Org_Type,
+            'Laureate_Full_Name': laureate.Laureate_Full_Name,
+            'Laureate_Known_Name': laureate.Laureate_Known_Name,
+            'Birth_Date': laureate.Birth_Date.strftime("%m/%d/%Y") if laureate.Birth_Date is not None else None,
+            'Birth_City': laureate.Birth_City,
+            'Birth_Country': laureate.Birth_Country,
+            'Birth_Lat': laureate.Birth_Lat,
+            'Birth_Lon': laureate.Birth_Lon,
+            'Gender': laureate.Gender,
+            'Age_When_Awarded': laureate.Age_When_Awarded,
+            'Award_id': award.Award_id,
+            'Category': award.Category,
+            'Affiliation_Name': award.Affiliation_Name,
+            'Award_year': award.Award_year,
+            'Motivation': award.Motivation,
+            'Date_Awarded': award.Date_Awarded.strftime("%m/%d/%Y") if award.Date_Awarded is not None else None,
+            'Award_City': award.Award_City,
+            'Award_Country': award.Award_Country,
+            'Award_Lat': award.Award_Lat,
+            'Award_Lon': award.Award_Lon,
+        } for laureate, award in joined_data
+    ]
+
+    return jsonify({'LaureatesAndAwards': merged_list})
+
+
+#------------------------------------------------------------------------
+#                       All Tables App route
+#------------------------------------------------------------------------
+
+
+@app.route("/AllTables")
+def get_AllTables():
+    # Perform a join operation to get data from Laureates, Awards, Prizes, and Orgs tables
+    # Assuming there are foreign key relationships between these tables
+    joined_data = (
+        db.session.query(Laureate, Award, Prize, Org)
+        .filter(Laureate.Laureate_id == Award.Laureate_id)
+        .filter(Laureate.Laureate_id == Prize.Laureate_id)
+        .filter(Laureate.Laureate_id == Org.Laureate_id)
+        .all()
+    )
+
+    # Format the joined data for JSON response
+    merged_list = [
+        {
+            'Laureate_id': laureate.Laureate_id,
+            'Org_Type': laureate.Org_Type,
+            'Laureate_Full_Name': laureate.Laureate_Full_Name,
+            'Laureate_Known_Name': laureate.Laureate_Known_Name,
+            'Birth_Date': laureate.Birth_Date.strftime("%m/%d/%Y") if laureate.Birth_Date is not None else None,
+            'Birth_City': laureate.Birth_City,
+            'Birth_Country': laureate.Birth_Country,
+            'Birth_Lat': laureate.Birth_Lat,
+            'Birth_Lon': laureate.Birth_Lon,
+            'Gender': laureate.Gender,
+            'Age_When_Awarded': laureate.Age_When_Awarded,
+            'Award_id': award.Award_id,
+            'Category': award.Category,
+            'Affiliation_Name': award.Affiliation_Name,
+            'Award_year': award.Award_year,
+            'Motivation': award.Motivation,
+            'Date_Awarded': award.Date_Awarded.strftime("%m/%d/%Y") if award.Date_Awarded is not None else None,
+            'Award_City': award.Award_City,
+            'Award_Country': award.Award_Country,
+            'Award_Lat': award.Award_Lat,
+            'Award_Lon': award.Award_Lon,
+            'Prize_id': prize.Prize_id,
+            'Prize_Amount': prize.Prize_Amount,
+            'Prize_Amount_Adj': prize.Prize_Amount_Adj,
+            'Sole_Winner': prize.Sole_Winner,
+            'Portion': prize.Portion,
+            'Org_id': org.Org_id,
+            'Organization_Name': org.Organization_Name,
+            'Founded_Country': org.Founded_Country,
+            'Org_Founded_Date': org.Org_Founded_Date.strftime("%m/%d/%Y") if org.Org_Founded_Date is not None else None,
+        }
+        for laureate, award, prize, org in joined_data
+    ]
+
+    return jsonify({'LaureatesAndAwards': merged_list})
 #------------------------------------------------------------------------
 #========================================================================
 
